eeb6cc9b1bfba44b90ea94b3777fb3d80b955757
Matrin Grillo <m.grillo@pharmatechnik.de>
2023-01-14 18:10:21 +0100

    und los

---
+++ secrets.py
@@ -0,0 +1,9 @@
+secrets = {
+    'ssid': 'GRITZ!Box 7490',
+    'password': '32110145812844357672',
+}
+secretsmgrillo = {
+    'url': 'mgrillo.de',
+    'ben': 'root',
+    'password': '28753315845',
+}
