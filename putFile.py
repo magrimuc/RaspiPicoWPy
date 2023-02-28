712002b7ed3b0f1b2adabd7d5f144b7c14e857e1
Matrin Grillo <m.grillo@pharmatechnik.de>
2023-02-28 17:27:30 +0100

    Beim senden endlich alles richtig machen. keinen SChrott auf den Socket, wenn eine 180 kb jpg DAtei drüber geht, kommt die an

--- putFile.py
+++ putFile.py
@@ -1,32 +1,44 @@
 import socket
 
 def putFile():
-# Initialize Socket Instance
+    # Initialize Socket Instance
     sock = socket.socket()
     print ("Socket created successfully.")
-    
+
     # Defining port and host
     port = 8800
-    host = '5.230.70.234'
-    ##host = 'localhost'
-    
+    host = 'mgrillo.de'
+
     # Connect socket to the host and port
     sock.connect((host, port))
     print('Connection Established.')
     # Send a greeting to the server
-    sock.send('Hallo  '.encode())
-    
-    # file read
-    file = open('cfile.txt', 'rb')
+    # sock.send('A message from the client'.encode())
+
+    # Write File in binary
+    file = open('server-file.txt', 'rb')
     line = file.read(1024)
-    # file send
+    # Keep sending data to the client
     while(line):
         sock.send(line)
         line = file.read(1024)
-    
-    print('File has been received successfully.')
-    
+
+    print('File has been sent successfully.')
+
     file.close()
     sock.close()
     print('Connection Closed.')
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
